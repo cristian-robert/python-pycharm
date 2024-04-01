a8129281615a10f83f4587b38360ef2a19620f0d
Cristian-Robert Iosef <rooobyrm999@gmail.com>
2024-04-01 19:05:24 +0300

    wip

--- seleniumFw/conftest.py
+++ seleniumFw/conftest.py
@@ -4,11 +4,10 @@ from selenium import webdriver
 import os
 from seleniumFw.src.helpers.config_helpers import get_base_url
 import logging
-import sys
 
 import pytest
 
-from reportportal_client import RPLogger
+from reportportal_client import RPLogger, RPLogHandler
 
 
 @pytest.fixture(scope="function")
@@ -50,47 +49,48 @@ def pytest_runtest_makereport(item, call):
     outcome = yield
     report = outcome.get_result()
     extras = getattr(report, "extras", [])
-    if report.when == "call":
-        xfail = hasattr(report, "wasxfail")
+
+    if report.when == 'call':
+        xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
-            if_frontend_test = True if 'init_driver' in item.fixturenames else False
+            if_frontend_test = 'init_driver' in item.fixturenames
             if if_frontend_test:
                 results_dir = os.environ.get('RESULTS_DIR')
-                print(f"results_dir: {results_dir}")
                 if not results_dir:
                     results_dir = os.path.join(os.getcwd(), 'results')
                     print(f"results_dir not set, defaulting to: {results_dir}")
-                screenshot_file = os.path.join(results_dir, item.name + ".png")
+
+                screenshot_file = os.path.join(results_dir, f"{item.name}.png")
                 driver = item.cls.driver
+
                 try:
                     driver.save_screenshot(screenshot_file)
-                except Exception as e:
-                    print(f"Error occurred while saving screenshot: {str(e)}")
-                extras.append(pytest_html.extras.image(screenshot_file))
-        report.extras = extras
 
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
-    if report.when == "call":
-        xfail = hasattr(report, "wasxfail")
-        if (report.skipped and xfail) or (report.failed and not xfail):
-            if_frontend_test = True if "init_driver" in item.fixturenames else False
-            if if_frontend_test:
-                results_dir = os.environ.get("RESULTS_DIR")
-                if not results_dir:
-                    results_dir = os.path.join(os.getcwd(), 'results')
-                    print(f"results_dir not set, defaulting to: {results_dir}")
-                screenshot_file = os.path.join(results_dir, item.name + ".png")
-                driver = item.cls.driver
-                try:
-                    driver.save_screenshot(screenshot_file)
+                    # Add allure attachment
                     with open(screenshot_file, 'rb') as file:
                         allure.attach(file.read(), name="screenshot", attachment_type=allure.attachment_type.PNG)
+                    print("###############################")
+                    print("###############################")
+                    print("###############################")
+                    print("###############################")
+                    logging.setLoggerClass(RPLogger)
+                    rp_logger = logging.getLogger(__name__)
+                    rp_logger.setLevel(logging.DEBUG)
+                    rp_logger.addHandler(RPLogHandler())
+                    with open(screenshot_file, 'rb') as image_file:
+                        image_data = image_file.read()
+
+                    # noinspection PyArgumentList
+                    rp_logger.info("Screenshot on test failure",
+                                   attachment={
+                                       "name": f"{item.name}.png",
+                                       "data": image_data,
+                                       "mime": "image/png"
+                                   })
                 except Exception as e:
                     print(f"Error occurred while saving screenshot: {str(e)}")
+
+            report.extras = extras
 
     @pytest.fixture(scope="session")
     def rp_logger():
